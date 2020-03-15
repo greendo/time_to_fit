3d6b9c5a4723cfc05fce3067bc3e577e0b2cab39
Max <melnychuk.m.o@gmail.com>
2020-03-15 13:10:50 +0200

    change in button and phone_layout

--- lib/ui/elements/buttons.dart
+++ lib/ui/elements/buttons.dart
@@ -3,39 +3,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:time_to_fit/ui/screens/timer_menu/timer_menu_screen.dart';
 
-enum ButtonType { timerMenu, journalMenu }
-
 class Button extends StatelessWidget {
-  final ButtonType type;
   final String asset;
-
-  Button({Key key, @required this.type, @required this.asset})
-      : assert(type != null, "Type can't be null"),
-        assert(asset != null, "Asset can't be null"),
-        super(key: key);
-
-  Button.timerMenu({Key key})
-      : type = ButtonType.timerMenu,
-        asset = "assets/images/stopwatch.png",
-        super(key: key);
-
-  Button.journalMenu({Key key})
-      : type = ButtonType.journalMenu,
-        asset = "assets/images/stopwatch.png",
-        super(key: key);
-
+  final String text;
+  final Function navigate;
+  Button({@required this.asset, @required this.text, @required this.navigate});
   @override
   Widget build(BuildContext context) {
     //TODO: read all translated strings from local json later
-    String text;
-    if (type == ButtonType.timerMenu) {
-      text = "Timer";
-    } else if (type == ButtonType.journalMenu) {
-      text = "Training journal";
-    }
-
     return GestureDetector(
-        onTap: _navigate(context),
+        onTap: navigate,
         child: Column(
           children: <Widget>[
             Image.asset(asset),
@@ -43,20 +20,5 @@ class Button extends StatelessWidget {
                 margin: T.homeTextMargin, child: Text(text, style: T.homeText)),
           ],
         ));
-  }
-
-  Function _navigate(BuildContext context) {
-    Widget w;
-    switch (type) {
-      case ButtonType.timerMenu:
-        w = TimerMenuScreen();
-        break;
-      case ButtonType.journalMenu:
-        w = null;
-    }
-
-    return () {
-      Navigator.push(context, MaterialPageRoute(builder: (context) => w));
-    };
   }
 }
--- lib/ui/screens/home/phone_layout.dart
+++ lib/ui/screens/home/phone_layout.dart
@@ -3,6 +3,9 @@ import 'package:time_to_fit/ui/elements/buttons.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../timer_menu/timer_menu_screen.dart';
+
+
 class HomePhoneLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) => Scaffold(
@@ -10,7 +13,28 @@ class HomePhoneLayout extends StatelessWidget {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[Button.timerMenu(), Button.journalMenu()],
+          children: <Widget>[
+            Button(
+              navigate: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => TimerMenuScreen()));
+              },
+              text: 'Timer',
+              asset: 'assets/images/stopwatch.png',
+            ),
+            Button(
+              navigate: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            TimerMenuScreen())); //TrainingJournalScreen()
+              },
+              text: 'Training journal',
+              asset: 'assets/images/stopwatch.png',
+            ),
+          ], //Button.journalMenu()
         ),
       ));
 }
+
